0d1ac6ec4da15bc43b1e0abb259768cbef1c3972
taohong <taohong@hyperchain.cn>
2022-08-03 17:06:52 +0800

    计算器

--- view-algorithm/src/day01/计算器/Calculate.java
+++ view-algorithm/src/day01/计算器/Calculate.java
@@ -10,58 +10,58 @@ import java.util.Scanner;
  */
 public class Calculate {
 
-  private static BigDecimal count = BigDecimal.ZERO;
+    private static BigDecimal count = BigDecimal.ZERO;
 
-  private static Boolean flag = Boolean.TRUE;
+    private static Boolean flag = Boolean.TRUE;
 
-  public static void main(String[] args) {
-    System.out.println("输入+进行加法运算");
-    System.out.println("输入-进行减法运算");
-    System.out.println("输入*进行乘法运算");
-    System.out.println("输入/进行除法运算");
-    System.out.println("输入q退出计算");
-    System.out.println("输入c清零");
-    Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("输入+进行加法运算");
+        System.out.println("输入-进行减法运算");
+        System.out.println("输入*进行乘法运算");
+        System.out.println("输入/进行除法运算");
+        System.out.println("输入q退出计算");
+        System.out.println("输入c清零");
+        Scanner scanner = new Scanner(System.in);
 
-    while (flag) {
-      BigDecimal i = BigDecimal.ZERO;
-      BigDecimal j = BigDecimal.ZERO;
-      System.out.println("请输入操作类型：");
-      String s = scanner.next();
-      if (!s.equals("q") && !s.equals("c")) {
-        if (!count.equals(BigDecimal.ZERO)) {
-          i = count;
-        }else {
-          System.out.println("请输入第一个数：");
-          i = scanner.nextBigDecimal();
+        while (flag) {
+            BigDecimal i = BigDecimal.ZERO;
+            BigDecimal j = BigDecimal.ZERO;
+            System.out.println("请输入操作类型：");
+            String s = scanner.next();
+            if (!s.equals("q") && !s.equals("c")) {
+                if (!count.equals(BigDecimal.ZERO)) {
+                    i = count;
+                }else {
+                    System.out.println("请输入第一个数：");
+                    i = scanner.nextBigDecimal();
+                }
+                System.out.println("请输入第二个数：");
+                j = scanner.nextBigDecimal();
+            }
+            switch (s) {
+                case "+":
+                    count = i.add(j);
+                    break;
+                case "-":
+                    count = i.subtract(j);
+                    break;
+                case "*":
+                    count = i.multiply(j);
+                    break;
+                case "/":
+                    count = i.divide(j, 2, BigDecimal.ROUND_HALF_UP);
+                    break;
+                case "q":
+                    scanner.close();
+                    flag = Boolean.FALSE;
+                    System.out.println("计算结束");
+                    break;
+                case "c":
+                    count = BigDecimal.ZERO;
+                    break;
+            }
+            System.out.println("计算结果："+count);
         }
-        System.out.println("请输入第二个数：");
-        j = scanner.nextBigDecimal();
-      }
-      switch (s) {
-        case "+":
-          count = i.add(j);
-          break;
-        case "-":
-          count = i.subtract(j);
-          break;
-        case "*":
-          count = i.multiply(j);
-          break;
-        case "/":
-          count = i.divide(j, 2, BigDecimal.ROUND_HALF_UP);
-          break;
-        case "q":
-          scanner.close();
-          flag = Boolean.FALSE;
-          System.out.println("计算结束");
-          break;
-        case "c":
-          count = BigDecimal.ZERO;
-          break;
-      }
-      System.out.println("计算结果："+count);
-    }
 
-  }
+    }
 }
